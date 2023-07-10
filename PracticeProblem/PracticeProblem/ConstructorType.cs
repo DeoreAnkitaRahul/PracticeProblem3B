5bd41d7525e3279bd5be2241c9322bc0c858acd1
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-07-10 12:50:44 +0530

    [Ankita] Add : UC1-Types of Constructor

---
+++ PracticeProblem/PracticeProblem/ConstructorType.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProblem
+{
+    public class ConstructorType
+    {
+           public string name;
+            public int age;
+            //Default Constructor
+            public ConstructorType()
+            {
+                Console.WriteLine($"This is Default Constructor.....");
+            }
+            //Parameterized Constructor
+            public ConstructorType(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+            }
+            //To get the Output of the Parameterized Constructor.
+            public void Output()
+            {
+                Console.WriteLine($"name: {name}");
+                Console.WriteLine($"Age : {age}");
+            }
+
+            //Copy Constructor: The constructor which creates an object by copying variables from another object is called a copy constructor
+            public string car;
+            public string Hp;
+            public ConstructorType(string Model, string Hp)
+            {
+                this.car = Model;
+                this.Hp = Hp;
+            }
+            public ConstructorType(ConstructorType typ)
+            {
+                car = typ.car;
+                Hp = typ.Hp;
+            }
+        
+    }
+}
---
+++ PracticeProblem/PracticeProblem/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace PracticeProblem
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ConstructorType obj = new ConstructorType();
+            ConstructorType obj_One = new ConstructorType("Bruce", 33);
+            obj_One.Output();
+            //Copy Constructor
+            ConstructorType obj_Two = new ConstructorType("Ferrari", "485Hp");
+            ConstructorType construct = new ConstructorType(obj_Two);
+            Console.WriteLine("Copy Constructor : " + construct.car);
+            Console.WriteLine("Copy Constructor : " + construct.Hp);
+        }
+    }
+}
